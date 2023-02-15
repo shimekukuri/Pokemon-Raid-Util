874d1329f79ee11d548096a2d20e22cc900869fd
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-02-15 14:56:53 -0500

    -

--- models/User.ts
+++ models/User.ts
@@ -1,4 +1,3 @@
-import { timeStamp } from 'console';
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
@@ -7,6 +6,9 @@ const userSchema = new mongoose.Schema(
     password: { type: String, required: true },
     email: { type: String, required: true },
     isAdmin: { type: Boolean, required: true, default: false },
+    rating: { type: Number, required: true, default: 3 },
+    banned: { type: Boolean, default: false },
+    confirmedPokemon: [{ pokemon: String }],
   },
   {
     timestamps: true,
