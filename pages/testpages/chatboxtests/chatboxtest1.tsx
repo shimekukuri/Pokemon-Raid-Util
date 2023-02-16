e23b5a5651bdc28b31d3c58a9fc3d72b29794d63
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-02-15 22:26:16 -0500

    -

---
+++ pages/testpages/chatboxtests/chatboxtest1.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Chatbox from '@/components/chatbox/Chatbox';
+
+export default function Chatboxtest1() {
+  return (
+    <div className="flex w-full min-h-screen justify-center items-center">
+      <Chatbox></Chatbox>
+    </div>
+  );
+}
