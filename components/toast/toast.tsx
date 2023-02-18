5810876598bc7de498a2e0634b92a209ef750988
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-02-17 22:02:35 -0500

    Chatbox

--- components/toast/toast.tsx
+++ components/toast/toast.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Toast({ content }: { content: string }) {
   return (
-    <div className="toast relative max-h-60">
-      <div className="alert alert-info w-full break-words overflow-y-scroll">
+    <div className="toast relative max-h-60 py-1">
+      <div className="alert alert-info w-full break-words">
         <div className="w-full px-1 break-words break-all text-sm">
           {content}
         </div>
