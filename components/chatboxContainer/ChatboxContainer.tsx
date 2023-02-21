aa390714bceb4d831406691a49a413631e5465cf
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-02-21 13:43:35 -0500

    -

--- components/chatboxContainer/ChatboxContainer.tsx
+++ components/chatboxContainer/ChatboxContainer.tsx
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Chatbox from '../chatbox/Chatbox';
+import { drawerContext } from '@/Context/drawerContext';
+import ChatboxController from '../ChatboxController/ChatboxController';
+import { themeContext } from '@/Context/themeContext';
+
+interface chatboxstate {
+  id: number;
+  active: boolean;
+  messages: string[];
+  newMessage: number;
+  recent: Date | null;
+}
+
+const reducer = (action, payload) => {};
+
+const initialState: chatboxstate = {
+  id: 0,
+  active: false,
+  messages: [],
+  newMessage: 0,
+  recent: null,
+};
 
 export default function ChatBoxContainer() {
+  const { dState, setDSTate } = useContext(drawerContext);
+  const { themeState } = useContext(themeContext);
+
   return (
-    <div className="absolute bottom-0 right-0 flex justify-end items-end max-w-full overflow-x-scroll">
-      <div className="flex-1"></div>
+    <div
+      className={`fixed bottom-0 right-0 flex justify-end items-end bg-opacity-0 overflow-visible gap-2 ${
+        dState ? '-z-50' : ''
+      }`}
+      data-theme={themeState}
+    >
       <Chatbox />
       <Chatbox />
       <Chatbox />
       <Chatbox />
       <Chatbox />
       <Chatbox />
+      <Chatbox />
+      <ChatboxController />
     </div>
   );
 }
