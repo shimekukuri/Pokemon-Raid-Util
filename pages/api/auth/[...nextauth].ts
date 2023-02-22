61bdd78d6519f710995f5b6a1a1c6b3151623d73
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-02-22 14:58:20 -0500

    -

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,1 +1,44 @@
-export {};
+import NextAuth from 'next-auth';
+import User from '../../../models/User';
+import db from '@/utilities/db/db';
+import bcryptjs from 'bcryptjs';
+import CredentialProvider from 'next-auth/providers/credentials';
+
+export default NextAuth({
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user?._id) token._id = user._id;
+      if (user?.isAdmin) token.isAdmin = user.isAdmin;
+      return token;
+    },
+    async session({ session, token }) {
+      if (token?._id) session.user._id = token._id;
+      if (token?.isAdmin) session.user.isAdmin = token.isAdmin;
+      return session;
+    },
+  },
+  providers: [
+    CredentialProvider({
+      async authorize(credentials) {
+        await db.connect();
+        const user = await User.findOne({
+          email: credentials.email,
+        });
+        await db.disconnect();
+        if (user && bcryptjs.compareSync(credentials.password, user.password)) {
+          return {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            iamge: 'f',
+            isAdmin: user.isAdmin,
+          };
+        }
+        throw new Error('invalid password or email.');
+      },
+    }),
+  ],
+});
---
+++ utilities/db/db.ts
@@ -0,0 +1,40 @@
+import mongoose, { ConnectionStates } from 'mongoose';
+
+interface mongooseConnection {
+  isConnected?: ConnectionStates;
+}
+
+const connection: mongooseConnection = {};
+
+export const connect = async () => {
+  if (connection.isConnected) {
+    console.log('Already Connected');
+    return;
+  }
+
+  if (mongoose.connections.length > 0) {
+    connection.isConnected = mongoose.connections[0].readyState;
+    if (connection.isConnected === 1) {
+      console.log('Use Previous Connection');
+      return;
+    }
+    await mongoose.disconnect();
+  }
+  const db = await mongoose.connect(process.env.MONGO_DB_URI);
+  console.log('New Connection');
+  connection.isConnected = db.connections[0].readyState;
+};
+
+const disconnect = async () => {
+  if (connection.isConnected) {
+    if (process.env.NODE_ENV === 'production') {
+      await mongoose.disconnect();
+      connection.isConnected = 0;
+    } else {
+      console.log('disconnected');
+    }
+  }
+};
+
+const db = { connect, disconnect };
+export default db;
