29169436820e6b0b97329fac66603f5c1f754f41
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-02-25 21:41:52 -0500

    -

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import { Inter } from '@next/font/google';
 import Layout from '@/components/Layout/Layout';
 import Hero from '@/components/hero/Hero';
+import { useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  const [loggedIn, setLoggedIn] = useState();
+
   return (
     <Layout>
       <Hero background={'https://images3.alphacoders.com/128/1289473.png'} />
