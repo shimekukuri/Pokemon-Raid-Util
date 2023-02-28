81b736aa23c1ff72a621337e4d28bce859f9c487
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-02-27 21:19:45 -0500

    -

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,7 +2,7 @@ import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import db from '@/utilities/db/db';
 import User from '@/models/User';
-import { compare } from 'bcryptjs';
+import bcryptjs from 'bcryptjs';
 
 export default NextAuth({
   session: {
@@ -12,55 +12,38 @@ export default NextAuth({
     async jwt({ token, user }) {
       if (user?._id) token._id = user._id;
       if (user?.isAdmin) token.isAdmin = user.isAdmin;
-      console.log(token);
       return token;
     },
     async session({ session, token }) {
       if (token?._id) session.user._id = token._id;
       if (token?.isAdmin) session.user.isAdmin = token.isAdmin;
-      console.log(session);
       return session;
     },
   },
   providers: [
     CredentialsProvider({
-      id: 'credentials',
-      name: 'Credentials',
-      credentials: {
-        email: {
-          label: 'Email',
-          type: 'text',
-        },
-        password: {
-          label: 'Password',
-          type: 'password',
-        },
-      },
       async authorize(credentials) {
-        await db.connect();
-
-        // Find user with the email
-        const user = await User.findOne({
-          email: credentials?.email,
-        });
-
-        // Email Not found
-        if (!user) {
-          throw new Error('Email is not registered');
+        try {
+          await db.connect();
+          const user = await User.findOne({
+            email: credentials.email,
+          });
+          await db.disconnect();
+          if (
+            user &&
+            bcryptjs.compareSync(credentials.password, user.password)
+          ) {
+            return {
+              _id: user._id,
+              name: user.name,
+              email: user.email,
+              iamge: 'f',
+            };
+          }
+        } catch {
+          await db.disconnect();
         }
-
-        // Check hased password with DB hashed password
-        const isPasswordCorrect = await compare(
-          credentials.password,
-          user.hashedPassword
-        );
-
-        // Incorrect password
-        if (!isPasswordCorrect) {
-          throw new Error('Password is incorrect');
-        }
-
-        return user;
+        throw new Error('invalid password or email.');
       },
     }),
   ],
