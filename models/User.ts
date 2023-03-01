2a02184390781b784bc150494dde6066a0aa5b93
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-02-28 21:51:20 -0500

    -

--- models/User.ts
+++ models/User.ts
@@ -4,6 +4,7 @@ export interface user {
   name: string;
   password: string;
   email: string;
+  image: number;
   rating: number;
   banned: boolean;
   confirmedPokemon: string[];
@@ -17,10 +18,20 @@ const userSchema = new Schema<user>(
     name: { type: String, required: true },
     password: { type: String, required: true },
     email: { type: String, required: true },
+    image: { type: Number, required: true, default: 1 },
     rating: { type: Number, required: true, default: 3 },
     banned: { type: Boolean, default: false },
     confirmedPokemon: { type: [String], default: [] },
-    friends: [{ type: [String], required: false, default: [] }],
+    friends: {
+      type: [
+        {
+          name: { type: String },
+          image: { type: Number },
+          id: { type: String },
+        },
+      ],
+      default: [],
+    },
   },
   {
     timestamps: true,
