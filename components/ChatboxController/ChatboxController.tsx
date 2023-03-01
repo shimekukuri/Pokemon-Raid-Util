bfd9cbec7e1d7c3a4eb4914911d943eacea9277a
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-02-28 21:51:16 -0500

    -

--- components/ChatboxController/ChatboxController.tsx
+++ components/ChatboxController/ChatboxController.tsx
@@ -1,50 +1,115 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useReducer } from 'react';
+import {
+  CHATBOX_REDUCER_ACTIONS,
+  cbstate,
+} from '../chatboxContainer/ChatboxContainer';
 
 export default function ChatboxController({
-  openChat,
-  setOpenChat,
+  chatboxState,
+  chatboxDispatch,
 }: {
-  openChat: boolean;
-  setOpenChat: React.Dispatch<React.SetStateAction<boolean>>;
+  chatboxState: cbstate;
+  chatboxDispatch: any;
 }) {
+  useEffect(() => {
+    console.log(chatboxState.menu);
+  }, [chatboxState]);
+
   return (
     <div className="right-1 h-40 flex flex-col justify-end gap-2">
-      {openChat ? (
-        <label className="btn btn-circle swap swap-rotate">
-          <input type="checkbox" />
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="swap-off fill-current"
-          >
-            <path
-              fillRule="evenodd"
-              d="M4.804 21.644A6.707 6.707 0 006 21.75a6.721 6.721 0 003.583-1.029c.774.182 1.584.279 2.417.279 5.322 0 9.75-3.97 9.75-9 0-5.03-4.428-9-9.75-9s-9.75 3.97-9.75 9c0 2.409 1.025 4.587 2.674 6.192.232.226.277.428.254.543a3.73 3.73 0 01-.814 1.686.75.75 0 00.44 1.223zM8.25 10.875a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zM10.875 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875-1.125a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z"
-              clipRule="evenodd"
+      {chatboxState.menu ? (
+        <>
+          <label className="btn btn-circle swap swap-rotate">
+            {/* Friends */}
+            <input
+              type="checkbox"
+              checked={chatboxState.friends}
+              onChange={(v) =>
+                chatboxDispatch({
+                  type: CHATBOX_REDUCER_ACTIONS.FRIENDS,
+                  payload: v.target.checked,
+                })
+              }
             />
-          </svg>
 
-          <svg
-            className="swap-on fill-current"
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 512 512"
-          >
-            <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
-          </svg>
-        </label>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 24 24"
+              fill="none"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="swap-off"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
+              />
+            </svg>
+
+            <svg
+              className="swap-on fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 512 512"
+            >
+              <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+            </svg>
+          </label>
+          <label className="btn btn-circle swap swap-rotate">
+            {/* Chatbox */}
+            <input
+              type="checkbox"
+              checked={chatboxState.chatbox}
+              onChange={(v) =>
+                chatboxDispatch({
+                  type: CHATBOX_REDUCER_ACTIONS.CHATBOXES,
+                  payload: v.target.checked,
+                })
+              }
+            />
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="swap-off fill-current"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.804 21.644A6.707 6.707 0 006 21.75a6.721 6.721 0 003.583-1.029c.774.182 1.584.279 2.417.279 5.322 0 9.75-3.97 9.75-9 0-5.03-4.428-9-9.75-9s-9.75 3.97-9.75 9c0 2.409 1.025 4.587 2.674 6.192.232.226.277.428.254.543a3.73 3.73 0 01-.814 1.686.75.75 0 00.44 1.223zM8.25 10.875a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zM10.875 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875-1.125a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z"
+                clipRule="evenodd"
+              />
+            </svg>
+
+            <svg
+              className="swap-on fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 512 512"
+            >
+              <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+            </svg>
+          </label>
+        </>
       ) : (
         ''
       )}
       <label className="btn btn-circle swap swap-rotate">
         <input
           type="checkbox"
-          onChange={(v) => setOpenChat(v.target.checked)}
+          onChange={(v) => {
+            chatboxDispatch({
+              type: CHATBOX_REDUCER_ACTIONS.OPEN_MENU,
+              payload: v.target.checked,
+            });
+          }}
         />
 
         <svg
