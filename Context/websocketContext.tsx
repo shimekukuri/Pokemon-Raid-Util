4fd27311d876b5c73c7e2cc444e023cb6cd38295
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-03-03 22:06:41 -0500

    -

--- Context/websocketContext.tsx
+++ Context/websocketContext.tsx
@@ -1,19 +1,56 @@
-import React, { createContext, ReactNode, useEffect, useState } from 'react';
+import React, {
+  createContext,
+  ReactNode,
+  useEffect,
+  useState,
+  useRef,
+} from 'react';
+
+import { useSession } from 'next-auth/react';
 
 export const webSocketContext = createContext<any>(null);
+
+export interface socketStateInterface {
+  instances: {};
+}
 
 export default function WebSocketProvider({
   children,
 }: {
   children: ReactNode;
 }) {
-  const [socketState, setSocketState] = useState();
+  const [socketState, setSocketState] = useState<socketStateInterface>({
+    instances: {},
+  });
+  const ws = useRef(null);
 
   useEffect(() => {
-    const ws = new WebSocket('ws://localhost:5555');
-    ws.onopen = (event) => {
+    ws.current = new WebSocket('ws://localhost:5555');
+    let w = ws.current as WebSocket;
+    w.onopen = (event) => {
       console.log(event);
     };
+
+    w.onmessage = (event) => {
+      let e = JSON.parse(event.data);
+      switch (e.event) {
+        case 'register': {
+          w.send(JSON.stringify({ event: 'register', userID: '2341' }));
+          break;
+        }
+        case 'message': {
+          console.log(e);
+          console.log(e.message);
+          setSocketState({
+            ...socketState,
+            instances: { ...socketState.instances },
+          });
+          break;
+        }
+      }
+    };
+
+    return () => w.close();
   }, []);
 
   const value = {
