6bfc90a31ae66f0be5f5882a3dfcf7ca2d59bbbf
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-03-08 17:33:28 -0500

    -

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -3,7 +3,7 @@ import User from '../../../models/User';
 import db from '../../../utilities/db/db';
 
 const handler = async (req, res) => {
-  console.log(db);
+  console.log(process.env.MONGO_URI);
   await db.connect();
   if (req.method !== 'POST') {
     db.disconnect();
