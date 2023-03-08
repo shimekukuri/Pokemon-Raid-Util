3d34f368f330a3a3d785dbee1379fd36de64bae6
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-03-08 08:56:00 -0500

    -

--- pages/createLobby.tsx
+++ pages/createLobby.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import Layout from '@/components/Layout/Layout';
 
 export default function CreateLobby() {
-  return <Layout>CreateLobby</Layout>;
+  return <Layout>CreateLobbyh</Layout>;
 }
