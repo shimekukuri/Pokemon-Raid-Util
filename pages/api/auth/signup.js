f9fbf9ff36863bf62f5b6c012bfff0e326596270
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-03-08 13:25:38 -0500

    -

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -3,50 +3,47 @@ import User from '../../../models/User';
 import db from '../../../utilities/db/db';
 
 const handler = async (req, res) => {
-  res.status(200).send({ req: { body: req.body, method: req.method } });
-  // console.log(req.method, req.body);
-  // if (req.method !== 'POST') {
-  //   return;
-  // }
-  // const { name, email, password } = req.body;
-  // console.log(name, email, password);
-  // if (
-  //   !name ||
-  //   !email ||
-  //   !email.includes('@') ||
-  //   !password ||
-  //   password.trim().length < 3
-  // ) {
-  //   res.status(422).json({
-  //     message: 'Validation error',
-  //   });
-  //   return;
-  // }
-  // await db.connect();
-  // console.log();
-  // console.log(db);
+  console.log(req.method, req.body);
+  if (req.method !== 'POST') {
+    return;
+  }
+  const { name, email, password } = req.body;
+  console.log(name, email, password);
+  if (
+    !name ||
+    !email ||
+    !email.includes('@') ||
+    !password ||
+    password.trim().length < 3
+  ) {
+    res.status(422).json({
+      message: 'Validation error',
+    });
+    return;
+  }
+  await db.connect();
 
-  // const existingUser = await User.findOne({ email: email });
-  // console.log(existingUser);
-  // if (existingUser) {
-  //   res.status(422).json({ message: 'User exists already! ' });
-  //   await db.disconnect();
-  //   return;
-  // }
+  const existingUser = await User.findOne({ email: email });
+  console.log(existingUser);
+  if (existingUser) {
+    res.status(422).json({ message: 'User exists already! ' });
+    await db.disconnect();
+    return;
+  }
 
-  // const newUser = new User({
-  //   name,
-  //   email,
-  //   password: bcryptjs.hashSync(password),
-  // });
-  // console.log(newUser);
-  // const user = await newUser.save();
-  // await db.disconnect();
-  // res.status(201).send({
-  //   message: 'Created user!',
-  //   _id: user._id,
-  //   email: user.email,
-  // });
+  const newUser = new User({
+    name,
+    email,
+    password: bcryptjs.hashSync(password),
+  });
+  console.log(newUser);
+  const user = await newUser.save();
+  await db.disconnect();
+  res.status(201).send({
+    message: 'Created user!',
+    _id: user._id,
+    email: user.email,
+  });
 };
 
 export default handler;
