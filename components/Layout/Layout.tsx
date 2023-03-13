7af4a073df59de9af22d117dd1a89973ac7893a1
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-03-13 09:43:28 -0400

    -

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -26,7 +26,7 @@ export default function Layout({ children }: { children: ReactNode }) {
     }
     return arr;
   };
-  console.log(session.data.user.name);
+  console.log(session.data?.user.name);
 
   const drawerFooter = (
     <Select
