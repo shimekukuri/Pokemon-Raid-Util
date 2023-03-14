eb3c6167f1adda7f68b0b8a41268753daaadf8e6
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-03-14 09:34:41 -0400

    -

--- pages/api/user/get.ts
+++ pages/api/user/get.ts
@@ -1,5 +1,19 @@
-import { NextRequest, NextResponse } from 'next/server';
+import User from '@/models/User';
+import db from '@/utilities/db/db';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-const handler = async (req: NextRequest, res: NextResponse) => {};
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'POST') {
+    res.status(402).send({ message: 'Incorrect format' });
+  }
+
+  const { email } = req.body;
+
+  await db.connect();
+  const result = await User.findOne({ email: email });
+  await db.disconnect();
+
+  res.status(200).send({ user: result });
+};
 
 export default handler;
