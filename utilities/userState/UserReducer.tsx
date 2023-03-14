76373c817e86b75b84960bc0eb4a72d865024f20
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-03-14 09:34:52 -0400

    -

--- utilities/userState/UserReducer.tsx
+++ utilities/userState/UserReducer.tsx
@@ -1,18 +1,12 @@
-import {
-  useEffect,
-  useReducer,
-  createContext,
-  Children,
-  ReactNode,
-} from 'react';
+import { useEffect, useReducer, createContext, ReactNode } from 'react';
+import { useSession } from 'next-auth/react';
 import Cookies from 'js-cookie';
 import { user } from '@/models/User';
-import { rejects } from 'assert';
-import { create } from 'domain';
 
 export const USER_ACTIONS = {
   SET_DB_STATE: 'SET_DB_STATE',
   UPDATE_FRIENDS: 'UPDATE_FRIENDS',
+  SET_DEFAULT_STATE: 'SET_DEFAULT_STATE',
 };
 
 const checkedLoggedIn = (user: user): boolean => {
@@ -52,21 +46,87 @@ const reducerFunction = (state: user, action): user => {
         friends: [...state.friends, action.payload],
       };
     }
+    case USER_ACTIONS.SET_DB_STATE: {
+      return {
+        name: action.payload.name,
+        password: '',
+        email: action.payload.email,
+        image: action.payload.image,
+        rating: action.payload.rating,
+        banned: action.payload.banned,
+        confirmedPokemon: action.payload.confirmedPokemon,
+        bio: action.payload.bio,
+        friends: action.payload.friends,
+        posts: action.payload.posts,
+      };
+    }
+    case USER_ACTIONS.SET_DEFAULT_STATE: {
+      return {
+        name: '',
+        password: '',
+        email: '',
+        image: 1,
+        rating: 0,
+        banned: false,
+        confirmedPokemon: [],
+        bio: '',
+        friends: [],
+        posts: [],
+      };
+    }
   }
 };
-const userContext = createContext(null);
+export const userContext = createContext(null);
 
 export default function UserStateProvider({
   children,
 }: {
   children: ReactNode;
 }) {
+  const { data: session, status } = useSession();
   const [userState, userDispatch] = useReducer(
     reducerFunction,
     defaultState as user
   );
 
   useEffect(() => {
+    console.log(session);
+    if (status === 'authenticated') {
+      fetch('/api/user/get', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: session.user.email }),
+      })
+        .then((response) => response.json())
+        .then((r) => {
+          const data = r.user;
+          userDispatch({
+            type: USER_ACTIONS.SET_DB_STATE,
+            payload: {
+              name: data.name,
+              password: '',
+              email: data.email,
+              image: data.image,
+              rating: data.rating,
+              banned: data.banned,
+              confirmedPokemon: data.confirmedPokemon,
+              bio: data.bio,
+              friends: data.friends,
+              posts: data.posts,
+            },
+          });
+        });
+    }
+    if (status === 'unauthenticated') {
+      userDispatch({ type: USER_ACTIONS.SET_DEFAULT_STATE, payload: {} });
+    }
+  }, [status, session]);
+
+  useEffect(() => {
+    console.log(userState);
+    console.log(!checkedLoggedIn(userState));
     if (!checkedLoggedIn(userState)) {
       return;
     }
