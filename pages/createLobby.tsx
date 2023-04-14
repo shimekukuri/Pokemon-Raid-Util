c03994dc29be27f4d7d4ad598936038571218742
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-04-13 21:15:10 -0400

    -

--- pages/createLobby.tsx
+++ pages/createLobby.tsx
@@ -6,17 +6,23 @@ export default function CreateLobby() {
   return (
     <Layout>
       <div className="col-span-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 2xl:grid-cols-4 gap-2">
-        <div className="bg-primary rounded-2xl shadow-xl py-4 col-span-1">
+        <div className="bg-primary rounded-2xl shadow-xl py-4 col-span-1 hidden md:block">
           <div className="avatar rounded-full flex items-center justify-center">
             <div className="w-4/5 rounded-full bg-accent">
               <img src="https://images.alphacoders.com/121/1216117.jpg" />
             </div>
           </div>
         </div>
-        <div className="card glass lg:col-span-3 rounded-2xl shadow-xl">
-          {[...Array(9)].map((x, i) => {
-            return <div key={`ques${i}`}></div>;
-          })}
+        <div className="card glass lg:col-span-3 rounded-2xl shadow-xl flex">
+          <div className="flex flex-1 justify-between flex-col">
+            <div className="bg-primary-focus rounded-t-2xl h-12 px-4 bg-opacity-50">
+              y
+            </div>
+            <div className="flex-1">y</div>
+            <div className="bg-accent-focus bg-opacity-50 h-28 rounded-b-2xl px-4 py-2">
+              y
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
