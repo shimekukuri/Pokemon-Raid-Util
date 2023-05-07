8461586f3c90eea6fc40501c87ca71ab3092a060
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-05-07 12:20:17 -0400

    -

--- pages/article/[slug].tsx
+++ pages/article/[slug].tsx
@@ -3,28 +3,30 @@ import db from '@/utilities/db/db';
 import User from '@/models/User';
 import Layout from '@/components/Layout/Layout';
 
-export default function Aritcle({ user }) {
-  console.log(user);
+export default function Aritcle({ slug }) {
+  console.log(slug);
   return (
     <Layout>
-      <div>Profile</div>
+      <div>Test</div>
     </Layout>
   );
 }
 
 export async function getServerSideProps(context) {
   const { params } = context;
-  const { id } = params;
+  const { slug } = params;
 
-  await db.connect();
-  let data = await User.findOne({ name: id }).lean();
-  // @ts-expect-error
-  data._id = data._id.toString();
-  console.log(data);
-  await db.disconnect();
-  if (!data) {
-    return { props: { user: `Not Found` } };
-  }
+  // await db.connect();
+  // let data = await User.findOne({ name: id }).lean();
+  // // @ts-expect-error
+  // data._id = data._id.toString();
+  // console.log(data);
+  // await db.disconnect();
+  // if (!data) {
+  //   return { props: { user: `Not Found` } };
+  // }
 
-  return { props: { user: { _id: data._id, friends: data.friends } } };
+  // return { props: { user: { _id: data._id, friends: data.friends } } };
+
+  return { props: { slug: slug } };
 }
